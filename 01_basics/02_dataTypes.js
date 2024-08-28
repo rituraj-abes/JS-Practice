1310cd555e399ff1d1b122bfebf3ca64faf532fe
HETSON <115390762+rituraj-abes@users.noreply.github.com>
2024-08-28 05:57:55 +0000

    data types is disscussed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer JS version (by default)
+
+// alert("Hello")
+//we are using nodejs, not browser
+//different syntax available to use alert in Node
+
+// code readability should be taken care of
+console.log(3+3);
+console.log("RITU")
+
+// read mdn document
+// tc39.es -> original documentation
+
+let name = "Ritu"
+let age = 18
+let isLoggedIn = false
+
+/* 
+number range => 2 to power 53
+bigint => for very big number
+string => ""
+boolean => true/false
+null => standalone value(khali value hai)
+undefined => (value assign nhi hua hai)
+symbol => used to find uniqueness in react
+
+object
+*/
+
+console.log(typeof age); //number
+console.log(typeof null);       //object
+console.log(typeof undefined) //undefined
