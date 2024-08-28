c22654b26bbf60b5c27ea7ba157a0c5de0c98861
HETSON <115390762+rituraj-abes@users.noreply.github.com>
2024-08-28 06:17:24 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = "33"
+//string string number 33
+
+// let score = "33abc"
+//string string number NaN
+
+// let score = null
+//object object number 0
+
+// let score = undefined
+//undefined undefined number NaN
+
+//let score = true
+//boolean boolean number 1
+
+let score = "Ritu"
+// string string number NaN
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumver = Number(score)
+// console.log(typeof valueInNumver);
+// console.log(valueInNumver);
+
+//be carefull while converting the numbers as there is no strict check
+
+/*
+to convert in number => Number();
+to convert in string => String();
+to convert in boolean => Boolean();
+*/
+
+// For boolean
+// 1 , "Ritu" => true
+// 0 , ""     => false
