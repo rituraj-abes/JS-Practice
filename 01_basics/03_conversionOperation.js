8da9909ab22b6e5a0d81e4ed019f6c5c7ae2d819
HETSON <115390762+rituraj-abes@users.noreply.github.com>
2024-08-28 11:31:25 +0000

    conversion practice

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,3 +34,41 @@ to convert in boolean => Boolean();
 // For boolean
 // 1 , "Ritu" => true
 // 0 , ""     => false
+
+//****************   Operations   ****************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);    // 2 ka power 3
+console.log(2/2);     //quotient
+console.log(2%2);     //remainder
+*/
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+console.log(str3);
+
+/*
+these are not a good practice in production
+
+console.log("1" + 2);    //12
+console.log(1 + "2");    //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log(+true);   //1
+console.log(+"");     //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+*/
+
+let gameCounter = 100;
+gameCounter++;          //postfix
+++gameCounter;          //prefix
+console.log(gameCounter);
+
