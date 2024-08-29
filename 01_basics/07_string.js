0405fae546adddac814fa4223e95ab42f1349609
HETSON <115390762+rituraj-abes@users.noreply.github.com>
2024-08-29 21:21:15 +0000

    about string

---
+++ 01_basics/07_string.js
@@ -0,0 +1,11 @@
+// string can be denoted using "" OR ''
+// to concatinate : +
+console.log("hello" + "world");
+
+const name = 'ritu'
+const repoCount = 07
+// old way : not used now
+// console.log(name + " " + repoCount + " Value");
+
+//new way : use backticks ``
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
